e421c4bc315ea16710f18014cbfef30d93bec120
Danish-chouhan <111457988+Danish-chouhan@users.noreply.github.com>
2022-09-28 23:26:44 +1200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,55 @@
+document.write("hlw i m danish chouhan<br>");
+document.write(100);
+document.write("<i>hlw i m danish chouhan</i><br>");
+document.write("<b>hlw i m danish chouhan</b><br>");
+document.write("<strike>hlw i m danish chouhan</strike><br>");
+document.write("<u>hlw i m danish chouhan</u><br>");
+var a ="variable text tag<br>";
+document.write(a);
+var b ="100";
+document.write(b);
+document.write("variable numberic tags not usable in double cots<br>");
+
+var day = (6)
+
+switch (day) {
+    case 0 :
+            document.write("today is monday");
+    break ;
+    case 1 :
+        document.write("today is tuseday");
+    break ;
+    case 2:
+        document.write("today is wednesday");
+    break ;
+    case 3 :
+        document.write("today is thursday");
+    break ;
+    case 4 :
+        document.write("today is friday");
+    break ;
+    case 5 :
+        document.write("today is saturday");
+    break ;
+    case 6 :
+        document.write("today is sunday");
+    break ;
+          default:
+            document.write("enter the valid day");                                               
+}
+document.write("<hr>")
+var aa = 16
+var bb = 14
+if( aa > bb ){
+    alert("a is greater")
+}else{
+    alert("b is greater")
+}
+var c = confirm("hlw how are you do you like our webpage or not if u like then press ok otherwise cancel")
+      
+   if(c){
+    alert("thanks")
+   }
+   else{
+    alert("sorry")
+   }

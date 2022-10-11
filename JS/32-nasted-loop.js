10ca126f2d60e114e4078b737c371671468cdfe3
Danish-chouhan <111457988+Danish-chouhan@users.noreply.github.com>
2022-10-11 23:01:06 +0530

    Add files via upload

---
+++ JS/32-nasted-loop.js
@@ -0,0 +1,30 @@
+for(var a = 1; a <= 5; a++){
+    for(var b = 1; b <= a; b++){
+        document.write(b + " ")
+    }
+    document.write("<br>")
+}
+document.write("<hr>")
+for( var a = 1; a <= 5; a++ ){
+    for(var b = 1; b <= a ; b++ ){
+        document.write(a + " ")
+    }
+    document.write("<br>")
+}
+document.write("<hr>")
+for( var a = 5; a >= 1; a-- ){
+    for(var b = 1; b <= a ; b++ ){
+        document.write(a + " ")
+    }
+    document.write("<br>")
+}
+document.write("<hr>")
+
+for( var a = 5; a >= 1; a-- ){
+    for(var b = a; b >= 1 ; b-- ){
+        document.write(b + " ")
+    }
+    document.write("<br>")
+}
+document.write("<hr>")
+
